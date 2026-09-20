474a4061ca0980281c7d11410e03ca7c2ce10d94
Ilham Kukuh Prakosa <56493086+ilham89@users.noreply.github.com>
2026-09-20 21:32:50 +0700

    Wait for Cloud Tasks queue-admin IAM propagation

--- tasks.tf
+++ tasks.tf
@@ -1,3 +1,8 @@
+resource "time_sleep" "cloud_tasks_queue_admin_propagation" {
+  depends_on      = [google_project_iam_member.ci_deployer_roles]
+  create_duration = "30s"
+}
+
 resource "google_cloud_tasks_queue" "blueprint" {
   project  = var.project_id
   location = var.region
@@ -15,7 +20,7 @@ resource "google_cloud_tasks_queue" "blueprint" {
     max_doublings = 5
   }
 
-  depends_on = [google_project_service.apis, google_project_iam_member.ci_deployer_roles]
+  depends_on = [google_project_service.apis, time_sleep.cloud_tasks_queue_admin_propagation]
 }
 
 resource "google_cloud_tasks_queue" "lessons" {
@@ -35,5 +40,5 @@ resource "google_cloud_tasks_queue" "lessons" {
     max_doublings = 5
   }
 
-  depends_on = [google_project_service.apis, google_project_iam_member.ci_deployer_roles]
+  depends_on = [google_project_service.apis, time_sleep.cloud_tasks_queue_admin_propagation]
 }
