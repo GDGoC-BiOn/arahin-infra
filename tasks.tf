18e3cf7e4c3f6eb770b57fb8a3e07f9f77a66994
Ilham Kukuh Prakosa <56493086+ilham89@users.noreply.github.com>
2026-09-20 21:19:45 +0700

    Provision durable blueprint Cloud Tasks queues

---
+++ tasks.tf
@@ -0,0 +1,39 @@
+resource "google_cloud_tasks_queue" "blueprint" {
+  project  = var.project_id
+  location = var.region
+  name     = "blueprint-generations"
+
+  rate_limits {
+    max_dispatches_per_second = 2
+    max_concurrent_dispatches = 2
+  }
+
+  retry_config {
+    max_attempts  = 5
+    min_backoff   = "5s"
+    max_backoff   = "300s"
+    max_doublings = 5
+  }
+
+  depends_on = [google_project_service.apis]
+}
+
+resource "google_cloud_tasks_queue" "lessons" {
+  project  = var.project_id
+  location = var.region
+  name     = "blueprint-lessons"
+
+  rate_limits {
+    max_dispatches_per_second = 4
+    max_concurrent_dispatches = 4
+  }
+
+  retry_config {
+    max_attempts  = 5
+    min_backoff   = "5s"
+    max_backoff   = "300s"
+    max_doublings = 5
+  }
+
+  depends_on = [google_project_service.apis]
+}
